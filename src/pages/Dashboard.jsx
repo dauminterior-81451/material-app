02c606d6b00ce56dae9f082d50dd8e537b14d82d
dauminterior-81451 <ddr076511@gmail.com>
2025-05-07 13:52:10 +0900

    Delete src directory

--- src/pages/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-function Dashboard() {
-  return (
-    <div>
-      <h2>대시보드</h2>
-    </div>
-  );
-}
-
-export default Dashboard;
--- src/pages/Login.jsx
+++
@@ -1,9 +0,0 @@
-function Login() {
-  return (
-    <div>
-      <h2>로그인 페이지</h2>
-    </div>
-  );
-}
-
-export default Login;

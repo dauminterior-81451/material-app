c55aab394e95d2c9b5aebba08531394c73efac09
dauminterior-81451 <ddr076511@gmail.com>
2025-05-07 13:59:59 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h1>다움 자재관리 시스템</h1>
-      <Login />
-      <Dashboard />
-    </div>
+    <Router>
+      <div>
+        <h1>다움 자재관리 시스템</h1>
+        <nav>
+          <ul>
+            <li><Link to="/login">Login 페이지</Link></li>
+            <li><Link to="/dashboard">Dashboard 페이지</Link></li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
-};
+}
 
 export default App;
